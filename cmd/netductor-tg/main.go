f42b8cd6a195080b7f828b105fbd6d400edc31e0
PavelNeyman <hunter.trojan@gmail.com>
2026-09-12 11:34:31 +0300

    tg pretty HTML: cmd/netductor-tg/main.go

--- cmd/netductor-tg/main.go
+++ cmd/netductor-tg/main.go
@@ -176,6 +176,15 @@ func runND(args ...string) string {
 
 func statusInline() string {
 	var b strings.Builder
+	if getLang() != "en" {
+		b.WriteString("📊 <b>Статус</b>
+
+")
+	} else {
+		b.WriteString("📊 <b>Status</b>
+
+")
+	}
 	host, _ := os.Hostname()
 	ru := getLang() != "en"
 	act, inact := "active", "inactive"
